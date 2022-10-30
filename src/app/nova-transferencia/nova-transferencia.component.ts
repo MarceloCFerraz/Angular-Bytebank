e548f327bd8413f11d371779fd0ff383681789b0
Marcelo Carvalho Ferraz <carvalhoferrazmarcelo@gmail.com>
2022-10-30 11:08:44 -0300

    criado componente de nova transferência e adicionado two way data binding para os campos de valor e destino para conseguir acessar os dados

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nova-transferencia',
   templateUrl: './nova-transferencia.component.html',
-  styleUrls: ['./nova-transferencia.component.css']
+  styleUrls: ['./nova-transferencia.component.scss']
 })
 export class NovaTransferenciaComponent implements OnInit {
 
+  valor: number;
+  destino: number;
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  transferir(){
+    console.log("Valor: ", this.valor);
+    console.log("Destino: ", this.destino);
   }
 
 }
